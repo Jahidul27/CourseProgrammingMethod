81b0f0b4f3b887a4d780ab0b28a4dc5f5dbf958d
Jahidul27 <43121246+Jahidul27@users.noreply.github.com>
2018-10-24 22:54:49 -0400

    Update main(parts a and b).cpp

--- Assignment 3/Problem 2/main(parts a and b).cpp
+++ Assignment 3/Problem 2/main(parts a and b).cpp
@@ -38,15 +38,10 @@ struct node* put(struct node* node, int key)
 // Thjs will search for a given key in a given BST
 struct node* search(struct node* root, int key)
 {
-    // Base Cases: root is null or key is present at root
     if (root == NULL || root->key == key)
         return root;
-
-    // Key is greater than root's key
     if (root->key < key)
         return search(root->right, key);
-
-    // Key is smaller than root's key
     return search(root->left, key);
 }
 
